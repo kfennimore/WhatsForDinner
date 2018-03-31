49189962da7d015cbec816cc32cf37aef2072d90
Karousha Fennimore <karousha@gmail.com>
2018-03-31 10:21:35 -0400

    first save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -46,7 +46,6 @@ export class MyApp {
   }
 
   openPage(page) {
-    // close the menu when clicking a link from the menu
     this.menu.close();
     // navigate to the new page if it is not the current page
     this.nav.setRoot(page.component);
